2ca8f7b7c35fde421412710f356c946982b1c8ba
jinqian.chen <chenjinqian@horizonenergy.com.cn>
2017-03-17 14:24:11 +0800

    apt-get add -y option.

--- shell_scripts/installOS.sh
+++ shell_scripts/installOS.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 #a project to install OS onece for all.
-sudo apt-get install zsh emacs ipython3 python3 ipython ipython-notebook ipython3-notebook
+sudo apt-get install -y zsh emacs ipython3 python3 ipython ipython-notebook ipython3-notebook
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo apt-get install clisp
-sudo apt-get install git subversion
-sudo apt-get install sbcl  #no slime here, or with overlap with emacs-one.
+sudo apt-get install -y clisp
+sudo apt-get install -y git subversion
+sudo apt-get install -y sbcl  #no slime here, or with overlap with emacs-one.
 #install  chinese input method.
-sudo apt-get install ibus-pinyin
+sudo apt-get install -y ibus-pinyin
 # fix emacs chinese input local envirement, by adding one line into .rc file.
 
 
-sudo apt-get install python-dev python3-dev
+sudo apt-get install -y python-dev python3-dev
 sudo easy_install pip
 pip install --upgrade pip
 sudo easy_install3 pip
 
-sudo apt-get install libatlas-base-dev gfortran #prepare for scipy
-sudo apt-get install libfreetype6-dev libxft-dev #prepare for matplotlib
+sudo apt-get install -y libatlas-base-dev gfortran #prepare for scipy
+sudo apt-get install -y libfreetype6-dev libxft-dev #prepare for matplotlib
 sudo pip install scipy matplotlib pandas virtualenv
 
-sudo apt-get install texlive-full
+sudo apt-get install -y texlive-full
 
-sudo apt-get install libpq-dev
+sudo apt-get install -y libpq-dev
 sudo pip install psycopg2
 sudo pip install pgsql   #database adapter.
 sudo pip install reportlib nose psutil
-sudo apt-get install postgresql-contrib
+sudo apt-get install -y postgresql-contrib
 sudo apt-get intall tree docker
-sudo apt-get install icedtea-plugin
-sudo apt-get install unixodbc-dev
+sudo apt-get install -y icedtea-plugin
+sudo apt-get install -y unixodbc-dev
 sudo pip install pyodbc
 sudo apt-get autoremove
 
